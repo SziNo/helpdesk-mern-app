2447aef1c7d17fef6eab261326cee41c9de13cae
SziNo <szino1848@gmail.com>
2022-10-14 15:22:56 +0200

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+const PORT = process.env.PORT || 8000
+
+// Connect to DB
+connectDB()
+
+const app = express()
+
+// Without these 2 lines I'd get back undefined instead of the actual data from Postman
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to the Support Desk API' })
+})
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
